f7672f8893c32185936648ba7c502054e96ae392
Filippo Rossi <rf.rossi.filippo@gmail.com>
2026-07-30 02:00:01 +0200

    Update menu-data.js

--- menu-data.js
+++ menu-data.js
@@ -8,9 +8,7 @@ const MENU_STRUCTURE = [
     href: "/math/index.html",
     children: [
       { label: "Appunti", href: "/math/appunti.html" },
-      { label: "Tutorati", href: "/math/tutorati.html" },
-      { label: "Algebra", href: "/math/algebra.html" },
-      { label: "Analisi", href: "/math/analisi.html" }
+      { label: "Tutorati", href: "/math/tutorati.html" }
     ]
   },
   {
@@ -24,6 +22,19 @@ const MENU_STRUCTURE = [
   },
   {
     label: "Work in Progress",
-    href: "/wip/index.html"
+    href: "prova1.html",
+    children: [
+
+      { label: "prova2", href: "https://potewww.github.io/potesplit/" },
+      { label: "prova3", href: "https://potewww.github.io/TV-tracker/" },
+      {
+        label: "prova4",
+        href: "prova.html",
+        children: [
+          { label: "prova5", href: "projects/potesplit/docs.html" }
+        ]
+      },
+      { label: "Prov6", href: "projects/altro.html" }
+    ]
   }
 ];
